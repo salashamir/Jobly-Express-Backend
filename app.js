f7b8ce43f12c283e207c2552badcd397ee32ebb4
salashamir <christophsal97@gmail.com>
2023-07-11 18:00:41 -0400

    added render url to cors

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://jobly-salashamir.onrender.com"],
+  })
+);
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
@@ -26,7 +30,6 @@ app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
 app.use("/jobs", jobsRoutes);
-
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
